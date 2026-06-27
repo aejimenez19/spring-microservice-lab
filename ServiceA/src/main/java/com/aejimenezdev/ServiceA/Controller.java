9d8e01ac1e1e76172cc3dc27c60b1a7b0245b04b
aejimenez19 <aejimenez19@gmail.com>
2026-06-26 21:28:11 -0500

    feat: ajustando controladores para solo tener helloword en cada servicio

--- ServiceA/src/main/java/com/aejimenezdev/ServiceA/Controller.java
+++ ServiceA/src/main/java/com/aejimenezdev/ServiceA/Controller.java
@@ -1,9 +1,11 @@
 package com.aejimenezdev.ServiceA;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/serviceA")
 public class Controller {
     @GetMapping("/helloWorld")
     public String helloWorld() {
--- ServiceB/src/main/java/com/aejimenezdev/ServiceB/Controller.java
+++ ServiceB/src/main/java/com/aejimenezdev/ServiceB/Controller.java
@@ -3,26 +3,16 @@ package com.aejimenezdev.ServiceB;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 @RestController
+@RequestMapping("/serviceB")
 public class Controller {
 
-    private final DiscoveryClient discoveryClient;
-    private final RestClient restClient;
-
-    public Controller(DiscoveryClient discoveryClient) {
-        this.discoveryClient = discoveryClient;
-        restClient = RestClient.create();
-    }
-
-    @GetMapping
+    @GetMapping("/helloWorld")
     public String helloWorld() {
-        ServiceInstance serviceInstance = discoveryClient.getInstances("ServiceA").get(0);
-        String url = serviceInstance.getUri().toString() + "/helloWorld";
-
-        String response = restClient.get().uri(url).retrieve().body(String.class);
-        return response;
+        return "Hello World from Service B!";
     }
 }
